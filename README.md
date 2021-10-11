0464317afe37c75595b2fce266bee34a65ffa14e
Thomas Cleugh <tom5572@live.com.au>
2021-10-11 21:03:38 +1000

    First two output graphs running for scenarios 1-4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Thomas-Cleugh-and-Jason-Jones-2504-2021-PROJECT2
+
+To run default simulations, run the discrete_event_simulation_project.jl file.
--- src/output.jl
+++ src/output.jl
@@ -66,18 +66,71 @@ function get_scenarios()::Vector{NetworkParameters}
                                     μ_vector = ones(20),
                                     P = zeros(20,20),
                                     Q = diagm(3=>ones(19), -19=>ones(3)),                             
-                                    p_e = vcat(1,zeros(19)),
+                                    p_e = vcat(1, zeros(19)),
                                     K = fill(5,20))
 
     return [scenario1, scenario2, scenario3, scenario4, scenario5]
 end
 
 """
-Plots the mean number of items in the total system as a function of λ for the given scenario.
+Plots the mean number of items in the total system and the proportion of jobs that are in orbit (circulating between nodes)
+as a function of λ for the given scenario.
 """
-function plot_mean_items(scenario::NetworkParameters)
+function plot_mean_and_proportion(scenario::NetworkParameters;
+                            max_time::Float64 = 10.0^7,
+                            scenario_label::String = "",
+                            lambda_range = 1:5)
+
+    mean_jobs, proportions = Float64[], Float64[]
+
+
+    for λi in lambda_range
+
+        in_queue_traj, in_transit_traj = Int[], Int[]
+
+        function record_traj(time::Float64, state::NetworkState) 
+            #println("time = $time, $(state.queues)")
+            #push!(time_traj, time)
+            push!(in_queue_traj, sum(state.queues))
+            push!(in_transit_traj, state.in_transit)
+            return nothing
+        end
+
+        params = NetworkParameters(L = scenario.L, 
+                                    gamma_scv = scenario.gamma_scv, 
+                                    λ = λi,
+                                    η = scenario.η,
+                                    μ_vector = copy(scenario.μ_vector),
+                                    P = copy(scenario.P),
+                                    Q = copy(scenario.Q),
+                                    p_e = copy(scenario.p_e),
+                                    K = copy(scenario.K)
+        )
+
+        simulate(params, max_time = max_time, call_back = record_traj)
+
+        total_in_system = sum(in_queue_traj) + sum(in_transit_traj)
+
+        push!(mean_jobs,  total_in_system / length(in_queue_traj))
+        push!(proportions, sum(in_transit_traj) / total_in_system)
+        
+    end    
     
-    max_time = 100.0 
+    display(plot(lambda_range, mean_jobs, title = "Mean number of jobs in system $scenario_label", xlabel = "λ", ylabel = "Mean num jobs", label = false))
+    display(plot(lambda_range, proportions, title = "Proportion of jobs in orbit $scenario_label", xlabel = "λ", ylabel = "Proportion", label = false))  
+
+end
+
+
+"""
+Plots the empirical distribution of the sojourn time of a job through the system (varied as a function of λ). for the given scenario.
+"""
+function plot_empirical_distribution(scenario::NetworkParameters, lambda_range)
+
+end
+
+function do_plots(scenario::NetworkParameters)
+    max_time = 100.0
     time_traj, queues_traj, in_transit_traj = Float64[], Vector{Int}[], Int[]
 
     function record_traj(time::Float64, state::NetworkState) 
@@ -114,29 +167,23 @@ function plot_mean_items(scenario::NetworkParameters)
     display(p)
 
     time_traj, queues_traj, in_transit_traj, params
-
 end
 
-"""
-Plots the proportion of jobs that are in orbit (circulating between nodes) as a function of λ. for the given scenario.
-"""
-function plot_proportion_in_orbit(scenario::NetworkParameters, lambda_range)
-
-end
-
-"""
-Plots the empirical distribution of the sojourn time of a job through the system (varied as a function of λ). for the given scenario.
-"""
-function plot_empirical_distribution(scenario::NetworkParameters, lambda_range)
-
-end
 
 function run_default_sims()
-    #lambda_range = 1:10 # Set here (needs to change)
 
     for scenario in get_scenarios()
-        plot_mean_items(scenario)
-    #    plot_proportion_in_orbit(scenario, lambda_range)
-    #    plot_empirical_distribution(scenario, lambda_range)
+        #do_plots(scenario)
     end
+
+    lambda_range = 0.1:0.1:5
+    simulation_time = 10.0^5
+
+    for (i, scenario) in enumerate(get_scenarios()[1:4])
+        plot_mean_and_proportion(scenario, 
+                                max_time = simulation_time,
+                                lambda_range = lambda_range,
+                                scenario_label = "scenario $i")
+    end
+
 end
